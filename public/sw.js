6ad1c3edc87840dadd4550632d58785406bf7eb2
Víctor del Val <victor.val.andres@gmail.com>
2019-03-03 10:55:01 +0100

    Use service worker to add a couple of files to cache

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,35 @@
-self.addEventListener('install', () => {
+const cacheName = 'app-files-v1';
+
+const filesToCache = [
+  '/',
+  '/main.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+]
+
+self.addEventListener('install', (event) => {
   console.log("Service worker!")
+
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        console.log("cache esta abierta");
+        return cache.addAll(filesToCache);
+      })
+  )
 })
+
+self.addEventListener('activate', (event) => {
+  console.log("el nuevo service worker se ha activado")
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  )
+});
